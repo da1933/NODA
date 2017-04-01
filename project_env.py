0a37449c390fc6b5046d4734978d2ec941a2c0a8
lren90 <lren90@gmail.com>
2017-04-01 12:06:29 -0400

    Updated pe file, added numeric features file

--- project_env.py
+++ project_env.py
@@ -6,6 +6,26 @@ def to_date(col):
     col.dropna(inplace=True)
     col = col[col!=0]
     col = col.astype(int)
-    converted = pd.to_datetime(col, format='%Y%m%d')
+    converted = pd.to_datetime(col, errors='coerce', format='%Y%m%d')
     return converted
     
+
+def getDfSummary(input_data):
+    summary = input_data.describe().transpose()
+    summary = summary.drop(['count'], axis=1)
+
+    number_distinct = input_data.unstack().groupby(level=0).nunique(dropna=True)
+    number_distinct.name = "number_distinct"
+
+    number_nan = input_data.isnull().sum()
+    number_nan.name = "number_nan"
+    
+    data_type = input_data.dtypes
+    data_type.name = "type"
+    
+    count = input_data.count()
+    count.name='valid_count'
+
+    output_data = pd.concat([data_type,summary, number_distinct, count, number_nan], axis=1)
+   
+    return output_data
