ce92e587f095b4c304c8cc6fc1046db30e3b0630
lren90 <lren90@gmail.com>
2017-04-06 17:52:28 -0400

    Retained UNIQUE_ID

--- numeric_features.py
+++ numeric_features.py
@@ -11,5 +11,5 @@ data_simple['TOT_NUM_DEF'] = data_simple.groupby('SYS_NBR')['DFDN_SEQ_NBR'].tran
 #Indicator Variable for whether there were multiple defendents
 data_simple['MULTIPLE_DEF_FLAG'] = np.where(data_simple['TOT_NUM_DEF'] > 1,1,0)
 
-df_num_features = data_simple[['TOT_NUM_DEF','MULTIPLE_DEF_FLAG','SCREENING_DAYS','POLICE_RPT_DAYS']]
+df_num_features = data_simple[['UNIQUE_ID','TOT_NUM_DEF','MULTIPLE_DEF_FLAG','SCREENING_DAYS','POLICE_RPT_DAYS']]
 df_num_features.to_csv('df_num_features.csv', index=False)
