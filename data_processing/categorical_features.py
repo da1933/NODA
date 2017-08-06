52139762f08ca63c0a1536dbaf50d06ec84aaaf9
Caroline Roper <ceroper@gmail.com>
2017-08-06 14:50:40 -0400

    Added the chcharge description for common charges

--- data_processing/categorical_features.py
+++ data_processing/categorical_features.py
@@ -26,6 +26,22 @@ merged_cat_filled['ARREST_CREDIT_CODE'].replace('00', 'NA', inplace=True)
 CGCD = pd.read_stata("source/cgcd-cln.dta")
 Code = pd.read_stata("source/code-cln.dta")
 
+#Convert class from number to description
+
+Class_names = Code[Code.code_type == 'CSCLCD']
+Class_names = Class_names[Class_names.code_code != '']
+Class_names.code_code = Class_names.code_code.astype(int)
+
+merged_cat_filled = pd.merge(merged_cat_filled, Class_names[['code_code', 'long_desc']], \
+        left_on='CHARGE_CLASS', right_on='code_code')
+
+merged_cat_filled.drop(['CHARGE_CLASS', 'code_code'], axis = 1, inplace=True)
+merged_cat_filled.rename(columns={"long_desc": "CHARGE_CLASS"}, inplace=True)
+
+merged_cat_filled.CHARGE_CLASS.replace('CASE CLASS CODES', 'NA', inplace=True)
+
+#Join in Charge Descriptions
+
 merged_cat_filled = pd.merge(merged_cat_filled, CGCD[['charge_code', 'charge_class', 'charge_desc']], \
                              left_on=['LEAD_CHARGE_CODE'], right_on=['charge_code'], how='left')
 
@@ -33,7 +49,18 @@ merged_cat_filled = pd.merge(merged_cat_filled, CGCD[['charge_code', 'charge_cla
 unmatched = merged_cat_filled.loc[merged_cat_filled['charge_code'].isnull() == True,'LEAD_CHARGE_CODE'].unique()
 merged_cat_filled.loc[merged_cat_filled['LEAD_CHARGE_CODE'].isin(unmatched),'LEAD_CHARGE_CODE'] = 'NA'
 
-merged_cat_filled.drop(['charge_code','charge_class','charge_desc'], axis = 1).to_csv('output/df_cat_features.csv',index=False)
+#Change Charge Desc to NA if it doesn't occur at least 100 times
+charge_desc = pd.DataFrame(merged_cat_filled.charge_desc.value_counts())
+rare_charge_descs = list(charge_desc[charge_desc.charge_desc <= 100].index)
+merged_cat_filled.loc[merged_cat_filled['charge_desc'].isin(rare_charge_descs),'charge_desc'] = 'NA'
+
+#Capitalize Charge Desc
+merged_cat_filled.rename(columns={"charge_desc": "CHARGE_DESC"}, inplace = True)
+
+#Altered this to not drop the charge_desc
+merged_cat_filled.drop(['charge_code','charge_class'], axis = 1).to_csv('output/df_cat_features.csv',index=False)
+
+#This is also where I should make the charge class a name instead of a number for easier model interpretation
 
 '''
 create features using one hot encoding
